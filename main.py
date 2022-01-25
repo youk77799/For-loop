92869f085830b4753d17f92a1e6320577e68c2fd
youk77799 <63965544+youk77799@users.noreply.github.com>
2022-01-25 20:09:13 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,37 @@
+print("First For")
+print("")
+# code start here
+for letter in "Giraffe Academy":
+    print(letter)
+
+print("")
+print("Second For")
+print("")
+# code start here
+friends = ["Sarah", "Sierra", "Stephenie"]
+for friend in friends:
+    print(friend)
+
+print("")
+print("3rd For")
+print("")
+# code start here
+for index in range(3, 10):
+    print(index)
+
+print("")
+print("4th For")
+print("")
+# code start here
+for index in range(len(friends)):
+    print(friends[index])
+
+print("")
+print("5th For")
+print("")
+# code start here
+for index in range(5):
+    if index == 0:
+        print("first Iteration")
+    else:
+        print("Not first")
